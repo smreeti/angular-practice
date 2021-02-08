eb144c822485c5a8e0f7a498ab29ea87afa1cbc4
smreeti <mool.smreeti@gmail.com>
2021-02-08 12:27:25 +0545

    add route configuration globally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {UserComponent} from './users/user/user.component';
+import {ServerComponent} from './servers/server/server.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    children: [
+      {
+        path: ':id/:name',
+        component: UserComponent
+      },
+    ]
+  },
+  {
+    path: 'servers',
+    component: ServerComponent
+  },
+  {
+    path: 'notfound',
+    component: PageNotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/notfound'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
